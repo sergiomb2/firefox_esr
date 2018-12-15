778c38816f0a6f909134d634fa80cf2b528f7ffe
Sérgio M. Basto <sergio@serjux.com>
2018-12-15 16:54:59 +0000

    Add missing sources and runme.txt

---
+++ SOURCES/enablelegacy-prefs.js
@@ -0,0 +1,19 @@
+// First line must be a comment.
+
+pref("general.config.filename", "enablelegacy.cfg");
+pref("general.config.obscure_value", 0);
+
+// Do not automatically upgrade legacy add-ons when upgrading the browser.
+// (Note: To avoid unwanted legacy -> WebExtension upgrades,
+//  visit about:config and set extensions.update.autoUpdateDefault to false ).
+pref("extensions.showMismatchUI", false);
+
+// When this preference is toggled, legacy add-ons are automatically re-enabled.
+// Non-restartless add-ons may require a restart of the browser.
+// Non-multiprocess-compatible add-ons may disable e10s.
+pref("extensions.legacy.enabled", true);
+pref("xpinstall.signatures.required", false);
+// (Note: uncommenting won't work because app/profile/firefox.js also sets them)
+
+// Maybe even the following, to allow manual execution of JS code in the global console.
+// pref("devtools.chrome.enabled", true);
